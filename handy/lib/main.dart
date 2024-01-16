4dde18b30cb758201219017eb5223f2d2271cab0
DESKTOP-55TL2NN\Administrator <xa_kanglt@sunseer.co.jp>
2024-01-16 11:19:04 +0800

    init

---
+++ handy/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Center(
+    child: Text('你好',textDirection: TextDirection.ltr,style: TextStyle(
+        color: Colors.red
+    ),),
+  ));
+}
+
+
